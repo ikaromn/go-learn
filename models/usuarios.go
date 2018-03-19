b5d2356ea2d85dde3df816a4ba4e725cc09d28b7
Ikaro Pinheiro Lopes <ikaropinheiro6@gmail.com>
2018-03-18 21:11:30 -0300

    Create a link between model and database table

--- models/usuarios.go
+++ models/usuarios.go
@@ -1,1 +1,13 @@
 package models
+
+import (
+	"github.com/ikaro/usuarios/lib"
+)
+
+type Usuarios struct {
+	ID int `db:"id" json:"id"`
+	Name string `db:"name" json:"name"`
+	Email string `db:"email" json:"email`
+}
+
+var UsuarioModel = lib.Sess.Collection("usuarios")
