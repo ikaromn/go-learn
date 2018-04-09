cc1e14ede3700c193daec9435e532a83e56f5da7
Ikaro Pinheiro <ikaropinheiro6@gmail.com>
2018-04-08 22:36:13 -0300

    Change list of user to the v1 group

--- routers/routes.go
+++ routers/routes.go
@@ -10,12 +10,12 @@ var App *echo.Echo
 func init() {
 	App = echo.New()
 
-	App.GET("/", controllers.Home)
 	App.GET("/add", controllers.Add)
 	App.GET("/update/:id", controllers.UpdateController)
 
 	api := App.Group("/v1")
 	api.POST("/create", controllers.Create)
+	api.GET("/list", controllers.Home)
 	api.DELETE("/delete/:id", controllers.Delete)
 	api.PUT("/update/:id", controllers.Update)
 }
